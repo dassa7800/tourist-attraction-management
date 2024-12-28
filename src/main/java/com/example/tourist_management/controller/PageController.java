3f2190e431033777d7575ac0dad44bd8de0d7cdc
Praveen <dassa7800@gmail.com>
2024-12-28 10:12:18 +0530

    tabs Gallery and About Sri Lanka

---
+++ src/main/java/com/example/tourist_management/controller/PageController.java
@@ -0,0 +1,29 @@
+package com.example.tourist_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/gallery")
+    public String showGallery(Model model) {
+        return "static/gallery";
+    }
+
+    @GetMapping("/about-sri-lanka")
+    public String showAboutSriLanka(Model model) {
+        return "static/about-sri-lanka";
+    }
+
+    @GetMapping("/faq")
+    public String showFaq(Model model) {
+        return "static/faq";
+    }
+
+    @GetMapping("/about-us")
+    public String showAboutUs(Model model) {
+        return "static/about-us";
+    }
+}
